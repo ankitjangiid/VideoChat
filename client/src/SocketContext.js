15d56b6aa3d85f18632ea123ca9aa3c2f2b57540
Ankit <ankitjangidrps@gmail.com>
2021-07-10 20:41:36 +0530

    Final commit

---
+++ client/src/SocketContext.js
@@ -0,0 +1,135 @@
+import React, { createContext, useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+import Peer from "simple-peer";
+
+const SocketContext = createContext();
+
+// link of our server
+// const socket = io("http://localhost:5000");
+
+const socket = io("https://videocall-reactapp.herokuapp.com/");
+
+const ContextProvider = ({ children }) => {
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+  const [stream, setStream] = useState();
+  const [name, setName] = useState("");
+  const [call, setCall] = useState({});
+  // this state is used to store the id
+  const [me, setMe] = useState("");
+
+  const myVideo = useRef(); // for our video
+  const userVideo = useRef(); // for other person's video
+  const connectionRef = useRef();
+
+  // useEffect is used to load program as soon as the page load
+  useEffect(() => {
+    // this is to ask the user for camera and audio permission
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((currentStream) => {
+        setStream(currentStream);
+
+        // stream for our call
+        myVideo.current.srcObject = currentStream;
+      });
+
+    //   socket.on is used to listen to something
+    // here we listen to 'me' which we have setup in server file(index.js) where it'll give us the id as soon as we have connection
+    // here user receive the id
+    socket.on("me", (id) => setMe(id));
+
+    // this is the socket when we call the user
+    socket.on("callUser", ({ from, name: callerName, signal }) => {
+      // here we set all the data about the call like who is calling, what is the signal strength and are we receiving the call or we answering the call
+      setCall({ isReceivingCall: true, from, name: callerName, signal });
+    });
+  }, []);
+
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    // here initiator means the person who is calling, here its false because we're receiving the call
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    // this is when we receive a call what should happen
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: call.from });
+    });
+
+    peer.on("stream", (currentStream) => {
+      // stream for the other person
+      userVideo.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    // this means the our current connection is equal to the current peer which is inside of this connection
+    connectionRef.current = peer;
+  };
+
+  const callUser = (id) => {
+    //   here initiator is true because we're the person who is calling
+    const peer = new Peer({ initiator: true, trickle: false, stream });
+
+    // this is when we make a call what should happen
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name,
+      });
+    });
+
+    peer.on("stream", (currentStream) => {
+      // stream for the other person
+      userVideo.current.srcObject = currentStream;
+    });
+
+    //   this is when user accept our call
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true);
+
+      peer.signal(signal);
+    });
+
+    // this means the our current connection is equal to the current peer which is inside of this connection
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+
+    //   once call ended we destroy that connection ref
+    connectionRef.current.destroy();
+
+    //   this reload the page once the call ended and provides user new id
+    window.location.reload();
+  };
+
+  return (
+    //   this is a socket component, here whatever we pass in value will be accessable to all the components
+    // as you can see we pass all the info so that we can use them else where
+    <SocketContext.Provider
+      value={{
+        call,
+        callAccepted,
+        myVideo,
+        userVideo,
+        stream,
+        name,
+        setName,
+        callEnded,
+        me,
+        callUser,
+        leaveCall,
+        answerCall,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export { ContextProvider, SocketContext };
---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,75 @@
+import React, { useContext } from "react";
+import { Grid, Typography, Paper, makeStyles } from "@material-ui/core";
+
+import { SocketContext } from "../SocketContext";
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: "550px",
+    [theme.breakpoints.down("xs")]: {
+      width: "300px",
+    },
+  },
+  gridContainer: {
+    justifyContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
+  },
+  paper: {
+    padding: "10px",
+    border: "2px solid black",
+    margin: "10px",
+  },
+}));
+
+const VideoPlayer = () => {
+  // here context will contain all the variable that we export on SocketContext file
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.gridContainer}>
+      {/* our Video */}
+      {/* here it means that if there is a stream only then run this */}
+      {/* this is because we don't want to show this immidiately when page run we want it to run when user accept our call */}
+      {stream && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {name || ""}
+            </Typography>
+            <video
+              playsInline
+              muted
+              ref={myVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+
+      {/* users Video */}
+      {/* only run if call is accepted and didn't end */}
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {call.name || "Name"}
+            </Typography>
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
+};
+
+export default VideoPlayer;
